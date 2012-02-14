e91a89faefc6e6ec86aeefbdcbe37693dea00e70
d1rk <d1rk@gmx.de>
2012-02-14 11:16:40 +0100

    Simplified Filter, now using Less class
    - using Less Class internally
    - Returns a Response-object
    - simplified overall-nesting structure

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -12,55 +12,25 @@ use lithium\action\Dispatcher;
 use lithium\core\Libraries;
 use lithium\storage\Cache;
 
-require dirname(__DIR__).'/libraries/lessphp/lessc.inc.php';
-
-/**
- * TODO: Make sure, that subfolders are possible (currently not)
- *
- */
 Dispatcher::applyFilter('run', function($self, $params, $chain) {
-	
-	if(strstr($params['request']->url, '.css')) {
-		
-		// look for a matching less file
-		$basename = basename($params['request']->url);
-		$css_file = "less/$basename";
-		$less_file = "less/$basename.less";
-		if(file_exists($less_file)) {
-			
-			header('Content-Type: text/css');
 
-			// if there's an up-to-date css file, serve it
-			if(file_exists($css_file) && filemtime($css_file) >= filemtime($less_file)) {
-				return file_get_contents($css_file);
-			}
-
-			try {
-				$less = new lessc($less_file);
-				$output = array(
-					'/**',
-					' * generated '.date('r'),
-					' * by li3_less/lessphp',
-					' * ',
-					' * @link https://github.com/glaszig/li3_less',
-					' * @link http://leafo.net/lessphp',
-					' */'
-				);
-				$output = join(PHP_EOL, $output) . PHP_EOL . $less->parse();
-			} catch(Exception $e) {
-				header('Content-Type: text/css', true, 500);
-				$output = "/* less compiler exception: {$e->getMessage()} */";
-			}
-
-			file_put_contents("less/$basename", $output);
-			return $output;
-
-		}
-
+	if(!strstr($params['request']->url, '.css')) {
+		return $chain->next($self, $params, $chain);
 	}
-		
-	return $chain->next($self, $params, $chain);
 
+	// look for a matching less file
+	$basename = basename($params['request']->url);
+	$less_path =  LITHIUM_APP_PATH.'/webroot/less';
+	$less_file = str_replace('.css', '.less', "$less_path/$basename");
+
+	if(!file_exists($less_file)) {
+		return $chain->next($self, $params, $chain);
+	}
+
+	// found, so we parse this file
+	$output = Less::file($less_file, array('cache' => true));
+	return new Response(array(
+		'body' => $output,
+		'headers' => array('Content-type' => 'text/javascript')
+	));
 });
-
-?>
